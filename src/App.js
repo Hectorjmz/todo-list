6d961ec3439a15ae938c43deb3f1398ae6aea1d7
Hector_J <hjmontemayor@gmail.com>
2021-09-26 21:10:11 -0400

    Add image post it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import postIt from './postPic.png';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import TodoForm from './form'
+import Todo from './todo.js'
+import { Container, Button, Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build todo app',
+      isCompleted: false
+    },
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text: text, isCompleted: false}]
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    let temp = [...todos]
+    temp.splice(index, 1)
+    setTodos(temp)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+      <div className="App">
+      <Container>
+        {todos.map((todo, i) => 
+        <Todo className='stuff' index={i} key={i} todo={todo} remove={removeTodo} />)}
+        <TodoForm className='stuff' addTodo={addTodo} />
+        <img src={postIt} className="position-absolute top-0 start-0" alt="logo" />
+      </Container>
+      </div>
+  </>
   );
 }
 
---
+++ src/form.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function TodoForm({addTodo}){
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue('')
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            className='input'
+            value={value}
+            placeholder='Add ToDo'
+            onChange={e => setValue(e.target.value)}
+            />
+        </form>
+    )
+
+}
+
+export default TodoForm
